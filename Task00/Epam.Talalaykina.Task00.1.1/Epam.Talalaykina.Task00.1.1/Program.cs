a8e715d39bad2bfaa8e956db669ee0c8dba4154d
greenpenguin <ismailovaelmira@bk.ru>
2019-07-02 06:48:05 +0400

    Просто пытаюсь понять, как это работает

---
+++ Task00/Epam.Talalaykina.Task00.1.1/Epam.Talalaykina.Task00.1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Epam.Talalaykina.Task00._1._1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            WorkWithConsoleClass workWithConsoleClass = new WorkWithConsoleClass();
+            FunctionClass functionClass = new FunctionClass();
+            
+            workWithConsoleClass.Output(functionClass.StringCreation(workWithConsoleClass.Input()));
+        }
+    }
+}
---
+++ Task00/Epam.Talalaykina.Task00.1.2/Epam.Talalaykina.Task00.1.2/WorkWithConsoleClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Epam.Talalaykina.Task00._1._2
+{
+    public class WorkWithConsoleClass
+    {
+        public int Input()
+        {
+            int number;
+            Console.WriteLine("Please, input a number that is greater than zero");
+            if (int.TryParse(Console.ReadLine(), out int num))
+            {
+                if (num > 0)
+                {
+                    number = num;
+                }
+                else
+                {
+                    throw new ArgumentException("The number must be greater than zero!");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("It's not an integer value!");
+            }
+            
+            return number;
+        }
+
+        public void Output(bool result)
+        {
+            if (result)
+            {
+                Console.WriteLine("This number is simple");
+            }
+            else
+            {
+                Console.WriteLine("This number isn't simple");
+            }
+
+        }
+    }
+}
