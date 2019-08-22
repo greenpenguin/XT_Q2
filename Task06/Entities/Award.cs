e11c1f62738e73cbe2dfcf09a08d8ff740de8966
greenpenguin <penguin.is.green@gmail.com>
2019-08-22 17:02:19 +0400

    no message

---
+++ Task06/Entities/Award.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Entities
+{
+    public class Award
+    {
+        public int Id { get; private set; }
+
+        public string Title { get; private set; }
+        
+        public Award(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Title}";
+
+        }
+    }
+}
---
+++ Task06/Entities/IStorable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Entities
+{
+    public interface IStorable
+    {
+        bool AddUser(User user);
+        bool AddAward(Award award);
+        bool AddUsersAwardRecord(int userId);
+        bool AddAwardToUser(int userId, int awardId);
+        
+        bool DeleteUser(User user);
+        bool DeleteAward(Award award);
+        bool DeleteUsersAwardRecord(int userId);
+        bool DeleteAwardToUser(int userId, int awardId);
+        
+        ICollection<User> GetAllUsers();
+        ICollection<Award> GetAllAwards();
+        ICollection<UserAndAwards> GetAllUsersAndAwards();
+    }
+}
